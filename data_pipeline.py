fb2036499c4b0d33e56ea85a75d0aad6a03f05a1
narihowie <howardshihhoward@gmail.com>
2017-10-30 22:54:57 -0400

    tf

--- data_pipeline.py
+++ data_pipeline.py
@@ -4,6 +4,10 @@ class DataPipeline:
         self.batch_size=batch_size
         self.max_memory=max_memory
         self.input_size = None
+        self.num_gester = None
 
     def get_input_size(self):
         return self.input_size
+
+    def get_output_size(self):
+        return self.num_gester
--- gesture_model.py
+++ gesture_model.py
@@ -6,6 +6,36 @@ from data_pipeline import DataPipeline
 class GestureRecognizer:
 
     def __init__(self, configuration):
+
         self.conf = configuration
         self.data_pipeline = DataPipeline(self.conf.batch_size, self.conf.max_memory)
-        self.initial_state = tf.placeholder(tf.float32, shape=[self.conf.batch_size,self.conf])
+        self.input_size = self.conf.data_pipeline.get_input_size
+        self.output_size = self.data_pipeline.get_output_size()
+        self.lstm = tf.contrib.rnn.BasicLSTMCell(self.conf.state_size)
+        self.weights = hp.weight_variables([self.conf.state_size,self.output_size])
+        self.biases = hp.bias_variables([self.output_size])
+
+        self.input = tf.placeholder(tf.float32, shape=[self.conf.batch_size,
+                                                               self.conf.max_memory,
+                                                               self.input_size])
+        self.initial_state = tf.placeholder(tf.float32, shape=[self.conf.batch_size,
+                                                                self.conf.state_size])
+        self.current_state = tf.placeholder(tf.float32, shape=[self.conf.batch_size,
+                                                                self.conf.state_size])
+        self.hidden_state = tf.placeholder(tf.float32, shape=[self.conf.batch_size,
+                                                               self.conf.state_size])
+        self.label = tf.placeholder(tf.int32, shape=[self.conf.batch_size, self.conf.max_memory])
+
+        self.zero_state = self.lstm.zero_state(batch_size=self.conf.batch_size, dtype=tf.float32)
+
+
+    def train(self):
+        for batch in self.input:
+            output,  = self.lstm(batch, )  #batch: max_mem X input_size
+
+
+
+
+
+
+
